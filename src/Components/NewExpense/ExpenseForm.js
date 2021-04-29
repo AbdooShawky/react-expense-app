b160cb340cbf3524479db0270caad60a157a4fdd
AbdooShawky <56520536+AbdooShawky@users.noreply.github.com>
2021-04-29 20:05:03 +0200

    fixing errors in chart components

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import NewExpense from "./NewExpense";
+
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: +enteredAmount,
+      amount: enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -87,7 +87,6 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
-      <button type='button' onClick={props.onCancel} >Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
@@ -12,19 +12,9 @@ const NewExpense = (props) => {
     props.onAddExpense(expenseData);
   };
 
-  const [isEditing, setIsEditng] = useState(false);
-
-  const startEditingHandler = () => {
-    setIsEditng(true);
-  }
-
-  const cancelHandler = () => {
-    setIsEditng(false);
-  }
   return (
     <div className='new-expense'>
-      {!isEditing && <button type="button" onClick={startEditingHandler}>Add New Expense.</button>}
-      {isEditing && <ExpenseForm onCancel={cancelHandler} onSaveExpenseData={saveExpenseDataHandler} />}
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
